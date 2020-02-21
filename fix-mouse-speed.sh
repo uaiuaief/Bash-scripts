3e333eb15c4c153a8f35c734371132dbfbecfc3e
Jonathan <jhonatasbn14@gmail.com>
2020-02-21 14:18:02 -0300

    Update fix-mouse-speed.sh

--- fix-mouse-speed.sh
+++ fix-mouse-speed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 prop=$(xinput | grep -Po "Razer Razer Abyssus\s*id=(\d+)" | sed 's/Razer Razer Abyssus\s*id=//g');
-number=$(xinput --list-props $prop | grep "Coordinate Transformation Matrix (142)" | grep -Po "\(\d+" | sed 's/(//g');
+number=$(xinput --list-props $prop | grep "Coordinate Transformation Matrix\s*\(\d+" | grep -Po "\(\d+" | sed 's/(//g');
 
 set -x
 xinput --set-prop $prop $number 2.000000, 0.000000, 0.000000, 0.000000, 2.000000, 0.000000, 0.000000, 0.000000, 1.000000;
